ff107f40ed91483807599d678383afb39f2a6c6c
zzx_company <2362180482@qq.com>
2021-06-25 18:33:29 +0800

    redis in action

--- db/src/test/java/cn/edu/djtu/db/RedisInActionTest.java
+++ db/src/test/java/cn/edu/djtu/db/RedisInActionTest.java
@@ -5,8 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SetOperations;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author zzx
@@ -25,33 +33,72 @@ public class RedisInActionTest {
     @Test
     void defineUsersAndInventory() {
 //        redisTemplate.opsForHash().put();
-        redisTemplate.opsForSet().add("inventory:27", "itemA");
-        System.out.println(redisTemplate.opsForSet().isMember("inventory:27", "itemA"));
-        redisTemplate.opsForSet().remove("inventory:27", "itemA");
+//        redisTemplate.opsForSet().add(inventoryKey, itemId);
+//        System.out.println(redisTemplate.opsForSet().isMember(inventoryKey, itemId));
+        System.out.println(redisTemplate.opsForSet().remove(inventoryKey, itemId));
     }
+    
+    int sellerId = 27;
+    String itemId = "itemA";
+    String inventoryKey = "inventory:" + sellerId;
+    String itemKey = itemId + "." + sellerId;
+    double price = 134.2;
     
     @Test
     void listItem() {
-        int sellerId = 27;
-        String itemId = "itemA";
-        String inventoryKey = "inventory:" + sellerId;
-        String itemKey = itemId + "." + sellerId;
-        double price = 134.2;
+        //模拟其中一个线程改变值，看是否抛出异常
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 5, 1, TimeUnit.MINUTES, 
+                new ArrayBlockingQueue<>(5));
+        executor.execute(this::transactionalAndPipelinedListItem);
+//        new Thread(this::transactionalAndPipelinedListItem).start();
+        executor.execute(this::transactionalAndPipelinedListItem);
+//        transactionalAndPipelinedListItem();
+    }
+
+    private void transactionalAndPipelinedListItem() {
+        AtomicBoolean executed = new AtomicBoolean(false);
+//        boolean executed = false;
         long end = System.currentTimeMillis() + 5000;
-        redisTemplate.executePipelined((RedisCallback<?>) connection -> {
-            while (System.currentTimeMillis() < end) {
-//                connection.watch(inventoryKey.getBytes());
-//                if (connection.sIsMember(inventoryKey.getBytes(), itemId.getBytes()) == null) {
-//                    connection.unwatch();
-//                    return null;
-//                }
+        redisTemplate.execute((RedisCallback<?>) connection -> {
+            //在一定时间内不断重试，超时或者执行完成则结束；
+            while (!executed.get()) {
+                if (System.currentTimeMillis() >= end) {
+                    executed.set(true);
+                    break;
+                }
+                connection.watch(inventoryKey.getBytes());
                 connection.multi();
                 connection.zAdd("market:".getBytes(), price, itemKey.getBytes());
                 connection.sRem(inventoryKey.getBytes(), itemId.getBytes());
                 connection.exec();
+                executed.set(true);
             }
-            
             return null;
-        });
+        }, true, true);
+    }
+    
+    @Test
+    void anonymousClassTest() {
+        AtomicInteger integer = new AtomicInteger(23);
+        final int[] count = {0};
+        new Runnable() {
+
+            @Override
+            public void run() {
+                integer.incrementAndGet();
+            }
+        };
+    }
+    
+    @Test
+    void setTest() {
+        int sellerId = 27;
+        String itemId = "itemA";
+        String inventoryKey = "inventory:" + sellerId;
+        SetOperations<String, Object> opsForSet = redisTemplate.opsForSet();
+        Long addCount = opsForSet.add(inventoryKey, itemId);
+        System.out.println("【add count】 " + addCount);
+        System.out.println("【is member】 " + opsForSet.isMember(inventoryKey, itemId));
+//        System.out.println("【deleted count】 " + opsForSet.remove("inventory:27", "itemA"));
     }
 }
