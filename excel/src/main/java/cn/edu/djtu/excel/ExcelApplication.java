7cd592cdf2d48855d48ee0592d1b148d527f35b3
zzx_company <2362180482@qq.com>
2021-09-30 11:41:17 +0800

    @Cacheable

--- excel/src/main/java/cn/edu/djtu/excel/ExcelApplication.java
+++ excel/src/main/java/cn/edu/djtu/excel/ExcelApplication.java
@@ -3,6 +3,7 @@ package cn.edu.djtu.excel;
 import cn.edu.djtu.excel.config.RibbonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableDiscoveryClient
 @RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 @EnableAsync
+@EnableCaching
 public class ExcelApplication {
 
     public static void main(String[] args) {
---
+++ excel/src/main/java/cn/edu/djtu/excel/entity/Book.java
@@ -0,0 +1,15 @@
+package cn.edu.djtu.excel.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author zzx
+ * @date 2021/9/17
+ */
+@Data
+@AllArgsConstructor
+public class Book {
+    private String isbn;
+    private String title;
+}
