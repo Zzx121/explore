f3c0023941cbd3cc92dfde6acfaa1e9115d69767
Zzx121 <38521340+Zzx121@users.noreply.github.com>
2025-04-26 04:13:50 +0000

    DFS to find path

--- experiments/python/findPath.py
+++ experiments/python/findPath.py
@@ -21,28 +21,35 @@ current_axis = start_axis
 moved_directions = {}
 # move from the start
 while(True):
-    moved_count = moved_directions.get(current_axis, 0)
     # if not in the pathes and not tried four directions, go on
     # the way of match int with the specific next move axis is not flexible
-    next_possible_axis = [(current_axis[0] + 1, current_axis[1]), (current_axis[0] - 1, current_axis[1]),
-                          (current_axis[0], current_axis[1] + 1), (current_axis[0], current_axis[1] - 1)]
-    can_move_axis = []
-    moved_list = moved_directions.get(current_axis)
-    if(moved_list == None):
-        moved_list = []
+    # next_possible_axis = [(current_axis[0] + 1, current_axis[1]), (current_axis[0] - 1, current_axis[1]),
+    #                       (current_axis[0], current_axis[1] + 1), (current_axis[0], current_axis[1] - 1)]
+    next_possible_axis = [(current_axis[0] - 1, current_axis[1]), (current_axis[0], current_axis[1] - 1),
+                          (current_axis[0] + 1, current_axis[1]), (current_axis[0], current_axis[1] + 1) ]
+    moved_list = moved_directions.get(current_axis, [])
     for p in next_possible_axis:
+        # priorized go through not moved axis, then go back when needed
+        # if((p not in pathes or len(moved_list) == 3) and p not in moved_list):
         if(p not in pathes and p not in moved_list):
             next_axis = p
+            # if(len(moved_list) == 3):
+            #     pathes.remove(current_axis)
+            moved_list.append(next_axis)
             break
     
-    next_axis = (next_x, next_y)
+    # need to go back
+    if(current_axis == next_axis):
+        for p in reversed(pathes):
+            if(p in next_possible_axis and p not in moved_list):
+                next_axis = p
+                pathes.remove(current_axis)
+                moved_list.append(next_axis)
+                break
+    
+    moved_directions[current_axis] = moved_list
+    
     next_sign = maze_axis.get(next_axis)
-    # construct to map with list values
-    moved_list = moved_directions.get(next_axis)
-    if(moved_list == None):
-        moved_list = []
-    moved_list.append(next_axis)
-    moved_directions[next_axis] = moved_list
     # when meet the end just return
     if(next_axis == end_axis):
         pathes.append(next_axis)
@@ -53,7 +60,8 @@ while(True):
         continue
     elif(next_sign == path):
         current_axis = next_axis
-        pathes.append(next_axis)
+        if(next_axis not in pathes):
+            pathes.append(next_axis)
         continue
         
 print(pathes)
