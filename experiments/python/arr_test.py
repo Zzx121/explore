538d54638dbd7047b0792a0850afcbb9a227ff6e
Zzx121 <38521340+Zzx121@users.noreply.github.com>
2025-08-05 05:47:48 +0000

    finish up the BFS

---
+++ experiments/python/arr_test.py
@@ -0,0 +1,8 @@
+# arr = ["abc", 1, (2, 3), 4.5, "xyz"]
+# print(arr[1:3])  # Output: [1, (2, 3)]
+# tuple1 = (1, 2, 3)
+# tuple1.append(4)  # This will raise an AttributeError since tuples are immutable
+
+a, b = (2, 3)
+print(a)  # Output: 2 3
+print(b)  # Output: 2 3
--- experiments/python/findPath.py
+++ experiments/python/findPath.py
@@ -1,5 +1,7 @@
 # read the maze into list with axis, map is more suitable
 from pathlib import Path
+import sys
+# print(sys.getrecursionlimit())
 
 
 lineCount = 1
@@ -121,14 +123,57 @@ def find_by_BFS(current_axis, pathes):
     # root = {"node": start_axis, "children": []}
     
     # print the final path
-tree = build_tree({"node": start_axis, "children": []}, [])
-print(tree)
+# tree = build_tree({"node": start_axis, "children": []}, [])
+# print(tree)
 # shortestPath = find_by_BFS(start_axis, None)
 # print(shortestPath)
 # maybe no need to build the tree, but just simulate the semontaneous iteration,
 # the concept of the global depth maybe needed
-def build_tree_with_depth(current_node, passed_axis, depth):
+def bfs_with_queue(passed_axis = [], queued_nodes = []):
     # 1.two set of axis, one for the transversed axis, one for the delt axis, when move to next level, 
     # need to process thoroughly for the current level, that's no left not processed transversed axises, maybe the stack is useful
     # 2. for the 'infinite' pathes, maybe the final path is valuable, so just copy the previous pathes
-    # when iterate to next level axis
+    # when iterate to next level axis, bring the pathes with the axis
+    # 3. each axis have those properties: 1) depth, 2) path from root
+    if queued_nodes == []:
+        current_node = {"axis": start_axis, "pathes": [start_axis]}
+    else:
+        current_node = queued_nodes.pop(0)    
+    current_axis = current_node["axis"]
+    next_possible_axis = [(current_axis[0] + 1, current_axis[1]), (current_axis[0] - 1, current_axis[1]),
+                          (current_axis[0], current_axis[1] + 1), (current_axis[0], current_axis[1] - 1)]
+    for n in next_possible_axis:
+        if((maze_axis.get(n) == path or maze_axis.get(n) == end) and n not in passed_axis):
+            passed_axis.append(n)
+            # otherwise, the pathes are not correct, need to not influence the parent pathes
+            current_pathes = current_node["pathes"].copy()
+            current_pathes.append(n)
+            queued_nodes.append({"axis": n, "pathes": current_pathes})
+            if(maze_axis.get(n) == end):
+                return current_pathes
+                
+    return bfs_with_queue(passed_axis, queued_nodes)
+
+def bfs_with_queue():
+    queued_nodes = []
+    passed_axis = []
+    while True:
+        if queued_nodes == []:
+            current_node = {"axis": start_axis, "pathes": [start_axis]}
+        else:
+            current_node = queued_nodes.pop(0)  
+        current_axis = current_node["axis"]
+        next_possible_axis = [(current_axis[0] + 1, current_axis[1]), (current_axis[0] - 1, current_axis[1]),
+                            (current_axis[0], current_axis[1] + 1), (current_axis[0], current_axis[1] - 1)]
+        for n in next_possible_axis:
+            if((maze_axis.get(n) == path or maze_axis.get(n) == end) and n not in passed_axis):
+                passed_axis.append(n)
+                current_pathes = current_node["pathes"].copy()
+                current_pathes.append(n)
+                queued_nodes.append({"axis": n, "pathes": current_pathes})
+                if(maze_axis.get(n) == end):
+                    return current_pathes
+                    
+# sys.setrecursionlimit(5000)
+path = bfs_with_queue()
+print(path)
